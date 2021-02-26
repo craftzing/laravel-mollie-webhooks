17480905c8bd85c072267db7f8d3c5e74d0a2480
ppelgrims <dev@pieterpelgrims.com>
2021-02-26 10:34:33 +0100

    Remove redundant sprintf fn

--- src/Exceptions/InvalidResourceId.php
+++ src/Exceptions/InvalidResourceId.php
@@ -6,15 +6,13 @@ namespace Craftzing\Laravel\MollieWebhooks\Exceptions;
 
 use Exception;
 
-use function sprintf;
-
 final class InvalidResourceId extends Exception
 {
     public static function missingExpectedPrefix(string $value, string $prefix): self
     {
         return new self(
-            "Value `$value` cannot be used as a Mollie resource identifier as it " .
-            sprintf("is missing the expected `%s` prefix.", $prefix),
+            "Value `$value` cannot be used as a Mollie resource identifier as it "
+            . "is missing the expected `$prefix` prefix."
         );
     }
 }
