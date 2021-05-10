357f6904c58a6fd90b7d5aa24063cf0fe95630ff
Rémi Pelhate <remi.pelhate@icloud.com>
2021-05-10 14:56:28 +0200

    Remove unused named constructors on ExposesRefundId

--- src/Events/ExposesRefundId.php
+++ src/Events/ExposesRefundId.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Craftzing\Laravel\MollieWebhooks\Events;
 
-use Craftzing\Laravel\MollieWebhooks\Orders\OrderId;
-use Craftzing\Laravel\MollieWebhooks\Payments\PaymentId;
 use Craftzing\Laravel\MollieWebhooks\Refunds\RefundId;
 use Craftzing\Laravel\MollieWebhooks\ResourceId;
 
@@ -25,21 +23,5 @@ trait ExposesRefundId
     {
         $this->refundId = $refundId;
         $this->resourceId = $resourceId;
-    }
-
-    /**
-     * @return static
-     */
-    public static function forPayment(PaymentId $paymentId, RefundId $refundId): self
-    {
-        return new static($refundId, $paymentId);
-    }
-
-    /**
-     * @return static
-     */
-    public static function forOrder(OrderId $orderId, RefundId $refundId): self
-    {
-        return new static($refundId, $orderId);
     }
 }
